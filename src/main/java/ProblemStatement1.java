c6c1dd602caf30fc730d72b9566dec3b423b18f7
Lucifer230101 <sauravgarje88@gmail.com>
2024-08-28 21:38:56 +0530

    minor changes in code

--- src/main/java/ProblemStatement1.java
+++ src/main/java/ProblemStatement1.java
@@ -26,30 +26,30 @@ public class ProblemStatement1 {
         }
 
         // Read JSON file and find the value of the first instance of "destination"
-        String destinationValue = findDestinationValue(jsonFilePath);
+        String destinationValue = extractDestinationValue(jsonFilePath);
         if (destinationValue == null) {
             System.out.println("Key 'destination' not found in the JSON file.");
             System.exit(1);
         }
 
         // Generate random 8-character alphanumeric string
-        String randomString = generateRandomString(8);
+        String randomAlphanumeric = createRandomString(8);
 
         // Concatenate PRN Number, Destination Value, and Random String
-        String concatenatedString = prnNumber + destinationValue + randomString;
+        String combinedString = prnNumber + destinationValue + randomAlphanumeric;
 
         // Generate MD5 hash
-        String md5Hash = generateMD5Hash(concatenatedString);
+        String md5Hash = computeMD5Hash(combinedString);
 
         // Output the result
-        System.out.println(md5Hash + ";" + randomString);
+        System.out.println(md5Hash + ";" + randomAlphanumeric);
     }
 
-    private static String findDestinationValue(String filePath) {
-        try (FileInputStream fis = new FileInputStream(filePath)) {
-            JSONTokener tokener = new JSONTokener(fis);
-            JSONObject jsonObject = new JSONObject(tokener);
-            return findDestinationValueRecursively(jsonObject);
+    private static String extractDestinationValue(String jsonFilePath) {
+        try (FileInputStream fileInputStream = new FileInputStream(jsonFilePath)) {
+            JSONTokener jsonTokener = new JSONTokener(fileInputStream);
+            JSONObject jsonObject = new JSONObject(jsonTokener);
+            return findDestinationValueInJson(jsonObject);
         } catch (IOException e) {
             System.out.println("Error reading the JSON file: " + e.getMessage());
             System.exit(1);
@@ -57,14 +57,14 @@ public class ProblemStatement1 {
         return null;
     }
 
-    private static String findDestinationValueRecursively(JSONObject jsonObject) {
+    private static String findDestinationValueInJson(JSONObject jsonObject) {
         for (String key : jsonObject.keySet()) {
             if (key.equals("destination")) {
                 return jsonObject.getString(key);
             }
             Object value = jsonObject.get(key);
             if (value instanceof JSONObject) {
-                String result = findDestinationValueRecursively((JSONObject) value);
+                String result = findDestinationValueInJson((JSONObject) value);
                 if (result != null) {
                     return result;
                 }
@@ -73,25 +73,25 @@ public class ProblemStatement1 {
         return null;
     }
 
-    private static String generateRandomString(int length) {
+    private static String createRandomString(int length) {
         String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        StringBuilder sb = new StringBuilder(length);
+        StringBuilder stringBuilder = new StringBuilder(length);
         Random random = new Random();
         for (int i = 0; i < length; i++) {
-            sb.append(characters.charAt(random.nextInt(characters.length())));
+            stringBuilder.append(characters.charAt(random.nextInt(characters.length())));
         }
-        return sb.toString();
+        return stringBuilder.toString();
     }
 
-    private static String generateMD5Hash(String input) {
+    private static String computeMD5Hash(String input) {
         try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            byte[] hashBytes = md.digest(input.getBytes(StandardCharsets.UTF_8));
-            StringBuilder sb = new StringBuilder();
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            byte[] hashBytes = messageDigest.digest(input.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hashBuilder = new StringBuilder();
             for (byte b : hashBytes) {
-                sb.append(String.format("%02x", b));
+                hashBuilder.append(String.format("%02x", b));
             }
-            return sb.toString();
+            return hashBuilder.toString();
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException("MD5 algorithm not found", e);
         }
